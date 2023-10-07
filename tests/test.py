89a6a2abe9f63b50da38ac465d883191e3de87dd
AlterAyrol <lazarev.reach@gmail.com>
2023-10-07 18:22:26 +0300

    добавил аллюр разделители

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,5 @@
 import allure
 from allure_commons.types import Severity
-import time
 from selene import be, have
 from selene import by
 
@@ -9,8 +8,15 @@ from STEK_demo_task.start_test_page import StartTestPage
 from STEK_demo_task.address_fond_page import AddressFondPage
 
 
+@allure.tag('district smoke check')
+@allure.label("owner", "AlterAyrol")
 class TestDemoCase:
 
+    @allure.severity(Severity.CRITICAL)
+    @allure.label("owner", "AlterAyrol")
+    @allure.epic('Тестирование вкладок в боковом меню')
+    @allure.feature('Реестр "Адресный фонд"')
+    @allure.story("Проверка кнопки реестра 'Адресный фонд' на кликабельность и соответствие текста кнопки")
     def test_element_is_available(self, web_browser):
         address_fond = 'Адресный фонд'
         addresses_of_residents = 'Адреса проживающих'
@@ -22,9 +28,6 @@ class TestDemoCase:
         start_page.enter_login(browser, site_login)
         start_page.enter_password(browser, site_password)
 
-        if browser.wait_until(have.text('Да')):
-            browser.element(by.text('Да')).click()
-
         start_page.check_text_address_fond(browser, text=address_fond)
         start_page.address_fond_clickable(browser)
 
@@ -33,6 +36,11 @@ class TestDemoCase:
         address_fond_page.check_text_address_fond(browser, text=addresses_of_residents)
         address_fond_page.address_fond_clickable(browser)
 
+    @allure.severity(Severity.CRITICAL)
+    @allure.label("owner", "AlterAyrol")
+    @allure.epic('Тестирование вкладок в боковом меню')
+    @allure.feature('Реестр "Адресный фонд"')
+    @allure.story("Создание нового района в реестре")
     def test_add_district(self, web_browser):
         new_district_name = 'new_district'
 
@@ -43,8 +51,6 @@ class TestDemoCase:
         start_page.enter_login(browser, site_login)
         start_page.enter_password(browser, site_password)
 
-        if browser.wait_until(have.text('Да')):
-            browser.element(by.text('Да')).click()
         start_page.address_fond_click(browser)
 
         address_fond_page.addresses_of_residents_click(browser)
@@ -54,6 +60,11 @@ class TestDemoCase:
 
         address_fond_page.check_text_district_on_page(browser, text=new_district_name)
 
+    @allure.severity(Severity.CRITICAL)
+    @allure.label("owner", "AlterAyrol")
+    @allure.epic('Тестирование вкладок в боковом меню')
+    @allure.feature('Реестр "Адресный фонд"')
+    @allure.story("Создание изменение названия района в реестре")
     def test_change_district_name(self, web_browser):
         change_district_name = 'changed district name'
 
@@ -64,8 +75,6 @@ class TestDemoCase:
         start_page.enter_login(browser, site_login)
         start_page.enter_password(browser, site_password)
 
-        if browser.element('//div[@class="v-card__actions"]/button[@type="button"]').wait_until(have.text('Да')):
-            browser.element('//div[@class="v-card__actions"]/button[@type="button"]').click()
         start_page.address_fond_click(browser)
 
         address_fond_page.addresses_of_residents_click(browser)
@@ -76,6 +85,11 @@ class TestDemoCase:
 
         address_fond_page.check_text_district_on_page(browser, text=change_district_name)
 
+    @allure.severity(Severity.CRITICAL)
+    @allure.label("owner", "AlterAyrol")
+    @allure.epic('Тестирование вкладок в боковом меню')
+    @allure.feature('Реестр "Адресный фонд"')
+    @allure.story("Удаление района в реестре")
     def test_delete_new_district(self, web_browser):
         browser, site_login, site_password = web_browser
         start_page = StartTestPage()
@@ -84,8 +98,6 @@ class TestDemoCase:
         start_page.enter_login(browser, site_login)
         start_page.enter_password(browser, site_password)
 
-        if browser.element('//div[@class="v-card__actions"]/button[@type="button"]').wait_until(have.text('Да')):
-            browser.element('//div[@class="v-card__actions"]/button[@type="button"]').click()
         start_page.address_fond_click(browser)
 
         address_fond_page.addresses_of_residents_click(browser)
@@ -94,6 +106,15 @@ class TestDemoCase:
         address_fond_page.delete_button_click(browser)
         address_fond_page.yes_button_click(browser)
 
+
+@allure.tag('logaut smoke check')
+@allure.label("owner", "AlterAyrol")
+class TestDemoCase:
+    @allure.severity(Severity.CRITICAL)
+    @allure.label("owner", "AlterAyrol")
+    @allure.epic('Тестирование меню пользователя в верхней навигационной панели')
+    @allure.feature('Меню пользователя')
+    @allure.story("Логаут из меню пользователя")
     def test_logout(self, web_browser):
         enter_text = 'Вход'
 
@@ -102,9 +123,6 @@ class TestDemoCase:
 
         start_page.enter_login(browser, site_login)
         start_page.enter_password(browser, site_password)
-
-        if browser.element('//div[@class="v-card__actions"]/button[@type="button"]').wait_until(have.text('Да')):
-            browser.element('//div[@class="v-card__actions"]/button[@type="button"]').click()
 
         start_page.user_menu_click(browser)
         start_page.exit_button_click(browser)
