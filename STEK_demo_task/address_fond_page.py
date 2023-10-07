cc1c0de78fdce8898895309b21b5538080ff97c2
AlterAyrol <lazarev.reach@gmail.com>
2023-10-07 11:42:32 +0300

    дописал локаторы и действия с ними

--- STEK_demo_task/address_fond_page.py
+++ STEK_demo_task/address_fond_page.py
@@ -1,6 +1,9 @@
 import allure
 # from selene import browser
 from selene import be, have
+from selene import by
+from selenium.webdriver import ActionChains, Keys
+from selene import command
 
 
 class AddressFondPage:
@@ -9,8 +12,13 @@ class AddressFondPage:
 
     addresses_of_residents_locator = '//a[@data-test-id="Адреса проживающих"]'
     add_something_new_button_locator = '//button[@data-cy="btn-add"]'
-    district_locator = '//div[@class="menuable__content__active"]/div[@data-cy="stack-menu-list"]/div[@data-cy="stack-menu-list-item"]'
-    district_name_input_field_locator = '#input-540'
+    district_locator_by_text = 'Район'
+    district_name_input_field_locator = '//input[@data-cy="stack-input"]'
+    table_locator = '//div[@class="v-data-table__wrapper"]/table'
+    edit_button_locator = '//div[@class="row no-gutters"]//button[@data-cy="btn-edit"]'
+    input_control_locator = '//div[@class="v-input__slot"]//div[@class="v-input--selection-controls__input"]'
+    delete_button_locator = '//button[@data-cy="btn-delete"]'
+    button_yes_and_no_locator = '//div[@data-test-id="stack-yes-no"]//div[@class="v-card__actions"]//button[@type="button"]'
 
     # Actions
 
@@ -21,12 +29,36 @@ class AddressFondPage:
         browser.element(self.add_something_new_button_locator).click()
 
     def district_click(self, browser):
-        browser.by(have.text('Район')).click()
+        browser.element(by.text(self.district_locator_by_text)).click()
 
     def district_name_input_field_add(self, browser, district_name):
         browser.element(self.district_name_input_field_locator).type(district_name).press_enter()
 
+    def check_text_district_on_page(self, browser, text):
+        browser.element(self.table_locator).should(have.text(text))
+
+    def address_fond_clickable(self, browser):
+        browser.element(self.addresses_of_residents_locator).should(be.clickable)
+
+    def check_text_address_fond(self, browser, text):
+        browser.element(self.addresses_of_residents_locator).should(have.text(text))
+
+    def edit_button_click(self, browser):
+        browser.all(self.edit_button_locator)[-1].click()
+
+    def select_all_text(self, browser):
+        browser.element(self.district_name_input_field_locator).perform(command.select_all)
+
+    def select_input_control(self, browser):
+        browser.all(self.input_control_locator)[-1].click()
+
+    def delete_button_click(self, browser):
+        browser.element(self.delete_button_locator).click()
+
+    def yes_button_click(self, browser):
+        browser.all(self.button_yes_and_no_locator).first.click()
 
 
 
+# Keys.COMMAND + 'a' + Keys.BACKSPACE + Keys.NULL
 
